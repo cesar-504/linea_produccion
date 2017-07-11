ec354a0cb4304dd4b122e52351deef35bb1278a3
Cesar Guerra <cesargb504@gmail.com>
2017-07-10 23:14:10 -0700

    registrar tipos ISender y SerialSender en qml

--- main.cpp
+++ main.cpp
@@ -7,15 +7,10 @@ int main(int argc, char *argv[])
 {
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
     QGuiApplication app(argc, argv);
+    qmlRegisterType<ISender>();
+    qmlRegisterType<SerialSender>("my", 1, 0, "SerialSender");
 
-    ISender *it ;
-    it = new SerialSender();
 
-    //QObject::connect(it,&ISender::msgReceived,[](QString m){qDebug() << "hi" <<m;});
-    //QObject::connect(it,&SerialSender::msgReceived,[](QString m){qDebug() << "hi Hi" <<m;});
-    QObject::connect(static_cast<SerialSender *>(it),&SerialSender::portNameChanged,[](){qDebug() << "hi hi Hi" ;});
-    dynamic_cast<SerialSender *>(it)->setPortName(" puerto ");
-    it->sendMsg(" Hola");
     QQmlApplicationEngine engine;
     engine.load(QUrl(QLatin1String("qrc:/main.qml")));
     if (engine.rootObjects().isEmpty())

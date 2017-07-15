4e8df5c12a8db35196e5f1ed3257d6f41730a9b7
Cesar Guerra <cesargb504@gmail.com>
2017-07-15 01:02:50 -0700

    splogmodel.cpp implementado

--- main.cpp
+++ main.cpp
@@ -16,28 +16,32 @@ int main(int argc, char *argv[])
     qmlRegisterType<ISender>();
     qmlRegisterType<SerialSender>("my", 1, 0, "SerialSender");
     qmlRegisterType<SPLogModel>("my", 1, 0, "SPLogModel");
-    ProductionLine  * p = new ProductionLine();
-    p->setSender(new TestSender());
-    QObject::connect(p,&ProductionLine::error,[](QString m ){qDebug() << "error " + m;});
-    QObject::connect(p,&ProductionLine::warning,[](QString m){qDebug() << "warning "+ m;});
-    QObject::connect(p,&ProductionLine::log,[](QString m){qDebug() << "log "+ m;});
-    QObject::connect(p,&ProductionLine::senderChanged,[](){qDebug() << "senderChanged";});
-    QObject::connect(p,&ProductionLine::isStartChanged,[](){qDebug() << "isStartChanged";});
-    QObject::connect(p,&ProductionLine::entryOn,[](int from, int to){qDebug() << "entry "+ QString::number( from) +","+ QString::number(to);});
-    QObject::connect(p,&ProductionLine::exitOn,[](int from, int to){qDebug() << "exit "+ QString::number( from) +","+ QString::number(to);});
+    qmlRegisterType<ProductionLine>("my", 1, 0, "ProductionLine");
+    qmlRegisterType<TestSender>("my", 1, 0, "TestSender");
+    ProductionLineDb::initDB();
+    ProductionLineDb::createTables();
+//    ProductionLine  * p = new ProductionLine();
+//    p->setSender(new TestSender());
+//    QObject::connect(p,&ProductionLine::error,[](QString m ){qDebug() << "error " + m;});
+//    QObject::connect(p,&ProductionLine::warning,[](QString m){qDebug() << "warning "+ m;});
+//    QObject::connect(p,&ProductionLine::log,[](QString m){qDebug() << "log "+ m;});
+//    QObject::connect(p,&ProductionLine::senderChanged,[](){qDebug() << "senderChanged";});
+//    QObject::connect(p,&ProductionLine::isStartChanged,[](){qDebug() << "isStartChanged";});
+//    QObject::connect(p,&ProductionLine::entryOn,[](int from, int to){qDebug() << "entry "+ QString::number( from) +","+ QString::number(to);});
+//    QObject::connect(p,&ProductionLine::exitOn,[](int from, int to){qDebug() << "exit "+ QString::number( from) +","+ QString::number(to);});
 
-    QObject::connect(p,&ProductionLine::started,[](int i){qDebug() << "started " + QString::number( i);});
-    QObject::connect(p,&ProductionLine::stopped,[](int i ){qDebug() << "stopped  "+ QString::number( i);});
+//    QObject::connect(p,&ProductionLine::started,[](int i){qDebug() << "started " + QString::number( i);});
+//    QObject::connect(p,&ProductionLine::stopped,[](int i ){qDebug() << "stopped  "+ QString::number( i);});
 
 
 
-    p->start();
-    p->stop();
-    p->start(-1);
-    p->start(2);
-    p->stop(-3);
-    p->stop(2);
-    p->stop(3);
+//    p->start();
+//    p->stop();
+//    p->start(-1);
+//    p->start(2);
+//    p->stop(-3);
+//    p->stop(2);
+//    p->stop(3);
 
 
 
--- splogmodel.cpp
+++ splogmodel.cpp
@@ -4,12 +4,11 @@
 #include <QSqlError>
 #include <QSqlRecord>
 #include <QSqlQuery>
-
-
+static const char *conversationsTableName = "StationProductLog";
 SPLogModel::SPLogModel(QObject *parent) : QSqlTableModel(parent)
 {
-    setTable("StationProductLog");
-    //setSort(1, Qt::DescendingOrder);
+    setTable(conversationsTableName);
+    setSort(0, Qt::DescendingOrder);
     setEditStrategy(QSqlTableModel::OnManualSubmit);
     select();
 }
@@ -25,7 +24,7 @@ QVariant SPLogModel::data(const QModelIndex &index, int role) const
 
 QHash<int, QByteArray> SPLogModel::roleNames() const
 {
-    static QHash<int, QByteArray> names;
+    QHash<int, QByteArray> names;
     names[Qt::UserRole] = "id";
     names[Qt::UserRole + 1] = "from";
     names[Qt::UserRole + 2] = "to";
@@ -34,3 +33,44 @@ QHash<int, QByteArray> SPLogModel::roleNames() const
     names[Qt::UserRole + 5] = "action";
     return names;
 }
+
+void SPLogModel::entry(const int from, const int to)
+{
+    entryExit(from,to,true);
+}
+
+void SPLogModel::exit(const int from, const int to)
+{
+    entryExit(from,to,false);
+}
+
+void SPLogModel::entryExit(const int from, const int to, bool isEntry)
+{
+//    QSqlQuery query;
+//    query.prepare("insert into StationProductLog(fromStation,toStation,id_Product,id_LogAction) "
+//                  "values (?,?,?,?)");
+//    query.addBindValue(from);
+//    query.addBindValue(to);
+//    query.addBindValue(1);
+//    query.addBindValue((isEntry)? 1: 2);
+//    if(!query.exec()){
+//        qDebug() << query.lastError();
+//        emit error(query.lastError().text());
+//    }
+    //const QString timestamp = QString::number( QDateTime::currentSecsSinceEpoch());
+    const QString timestamp = QDateTime::currentDateTime().toString(Qt::ISODate);
+    QSqlRecord rec = record();
+    rec.setValue("fromStation",from);
+    rec.setValue("toStation",to);
+    rec.setValue("id_Product",1);
+    rec.setValue("timeLog",timestamp);
+    rec.setValue("id_LogAction",(isEntry)? 1: 2);
+    qDebug() << rec.value(3);
+    if (!insertRecord(rowCount(), rec)) {
+        qWarning() << lastError().text();
+        emit error(lastError().text());
+        return;
+    }
+    bool a =submitAll();
+}
+
